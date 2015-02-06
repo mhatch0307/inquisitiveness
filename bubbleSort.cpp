667172efb0fc4c200bd2f2727bbd6cd0836a7831
Mike Nelson <mike.nelson.cs@gmail.com>
2015-02-06 14:44:02 -0700

    added bubbleSort.cpp

---
+++ bubbleSort.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+int ARRAY_SIZE = 20;
+
+void getRandomNumbers(int* numberList)
+{
+   srand(time(0));
+   for (int i = 0; i < ARRAY_SIZE; i++)
+   {
+      numberList[i] = rand() % 1001;
+   }      
+}
+
+void output(int* numberList)
+{
+   for (int i = 0; i < ARRAY_SIZE; i++)
+   {
+      cout << numberList[i] << " ";
+   }
+   cout << endl;
+}
+
+void bubbleSort(int* numberList)
+{
+   int temp;
+   bool sorted = false;
+   while (!sorted)
+   {
+      sorted = true;
+      for (int i = 0; i < ARRAY_SIZE - 1; i++)
+      {
+         if (numberList[i] > numberList[i+1])
+         {
+            temp = numberList[i];
+            numberList[i] = numberList[i+1];
+            numberList[i+1] = temp;
+            sorted = false;
+         }
+      }
+   }
+}
+
+int main()
+{
+   int numberList[ARRAY_SIZE]; 
+   getRandomNumbers(numberList);
+   bubbleSort(numberList);
+   output(numberList);
+   return 0;
+}
